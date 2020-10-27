f4b2aceca41857642aad8b0ed0dae64b00bbe0e7
Rodrigo Valente <valentergs@gmail.com>
2020-10-27 20:25:06 -0300

    Criar .env

--- driver/driver.go
+++ driver/driver.go
@@ -3,23 +3,32 @@ package driver
 import (
 	"database/sql"
 	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("arquivo .env náo encontrado")
+	}
+}
 
 //ConnectarBD conecta o programa ao Banco de Dados
 func ConnectarBD() *sql.DB {
 
 	var err error
 
-	const (
-		user     = "rodrigovalente"
-		password = "Gustavo2012"
-		// Quando rodar em Docker o HOST precisa ter o mesmo nome do container onde roda o Postgresql - nesse caso vai ficar postgres:5432
-		host   = "localhost"
-		port   = 5432
-		dbname = "valentedevBlog"
-	)
+	// const (
+	// 	user     = os.Getenv("BD_USUARIO")
+	// 	password = "Gustavo2012"
+	// 	// Quando rodar em Docker o HOST precisa ter o mesmo nome do container onde roda o Postgresql - nesse caso vai ficar postgres:5432
+	// 	host   = "localhost"
+	// 	port   = 5432
+	// 	dbname = "valentedevBlog"
+	// )
 
-	psqlInfo := fmt.Sprintf("user=%s password=%s host=%s port=%d dbname=%s sslmode=disable", user, password, host, port, dbname)
+	psqlInfo := fmt.Sprintf("user=%s password=%s host=%s port=%v dbname=%s sslmode=disable", os.Getenv("BD_USUARIO"), os.Getenv("BD_SENHA"), os.Getenv("BD_HOST"), os.Getenv("BD_PORTA"), os.Getenv("BD_NOME"))
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
